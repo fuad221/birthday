6f01411cc1616fca5360d6fc874a88a50d44d05f
galymax <werkhowski@gmail.com>
2021-03-17 01:56:46 +0100

    image fix

--- src/Interface/FormAndData/Interface.js
+++ src/Interface/FormAndData/Interface.js
@@ -1,16 +1,21 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import '../../App.css'
 
 
 const Interface = ({images, price, yourGift}) => { 
   const [img, setImg ] = useState(null)
   useEffect(() => { 
-    
-  })
+    if(images.indexOf("http") > -1){
+      setImg(images)
+    }else {
+      import("../../../public" + images)
+      .then(loaded => setImg(loaded.default))
+    }
+  }, [])
 
   return (
     <div className="cardCon">
-      <img src={images.indexOf("http") > -1 ? images : require("../../../public" + images)} alt =" "  className="imgSize" />
+      <img src={img} alt =" "  className="imgSize" />
       
       <p className="fontSize"> {`${price}$ get your discount 10%  ${yourGift}`}  </p>
 
