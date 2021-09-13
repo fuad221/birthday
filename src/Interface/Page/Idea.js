afb5996b9e46d0d31a93efecfd0a004be3e1e1a1
fuad <fuad221@outlook.com>
2021-09-13 13:22:13 +0200

    i added the adress

--- src/Interface/Page/Idea.js
+++ src/Interface/Page/Idea.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const Idea = () => {
-    return (
-        <div>
-          <h1>test Idea</h1>  
-        </div>
-    )
+  return (
+    <div>
+      <h1>New cooking and New brithday</h1>
+      <p>if you have an idea for new food or new surprise you can making in birthday.
+        contact us under this Email </p>
+
+      <address>
+        Written by <a href="mailto:cooking@cooking.com"> Chef Anton</a> <hr/>
+        <p>Vist us at </p> <br/>
+        <p>Banhofstra  20</p> <br/>
+        <p> 20098 Hamburg</p> <br/>
+
+      </address>
+
+
+    </div>
+  )
 }
 
 export default Idea
